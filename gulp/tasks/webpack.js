ea4da79503766e9ff7c38ae4936a33ef67d3dbc9
cliren <skatikaneni@climate.com>
2015-02-13 14:58:01 -0800

    docs

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -3,8 +3,9 @@ var webpack = require("webpack");
 var webpackConfig = require("../../webpack.config.js");
 var bundleLogger = require('../util/bundleLogger');
 
+var name = 'simple-component';
 gulp.task("webpack:build", function(callback) {
-  bundleLogger.start('PrecisionMap');
+  bundleLogger.start(name);
 
   var myConfig = Object.create(webpackConfig);
   myConfig.plugins = [
@@ -18,7 +19,7 @@ gulp.task("webpack:build", function(callback) {
 
   // run webpack
   webpack(myConfig, function(err, stats) {
-    bundleLogger.end('PrecisionMap');
+    bundleLogger.end(name);
     callback();
   });
 });
